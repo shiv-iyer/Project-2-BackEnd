5185ca8ffdcd927d085cbaccca8469813c226f83
Shiv <shiviyer55x@gmail.com>
2023-03-17 00:01:33 +0800

    sample data files

---
+++ sample-data/posts.js
@@ -0,0 +1,82 @@
+postMongoExample = {
+    // objectID is given naturally by Mongo
+    "name": "3.0 Xbow Cycle",
+    "userThatPosted": "reference object ID of user",
+    "dateOfCreation": "2023-03-16 (figure out date format later)",
+    "dateOfUpdation": null,
+    "deck": {
+        "cards": [{
+            "cardID": "reference object ID of card",
+            "cardImage": "reference within card",
+            "cardElixirCost": "reference within card"
+        },
+        {
+            "cardID": "reference object ID of card",
+            "cardImage": "reference within card",
+            "cardElixirCost": "reference within card"
+        },
+        {
+            "cardID": "reference object ID of card",
+            "cardImage": "reference within card",
+            "cardElixirCost": "reference within card"
+        },
+        {
+            "cardID": "reference object ID of card",
+            "cardImage": "reference within card",
+            "cardElixirCost": "reference within card"
+        },
+        {
+            "cardID": "reference object ID of card",
+            "cardImage": "reference within card",
+            "cardElixirCost": "reference within card"
+        },
+        {
+            "cardID": "reference object ID of card",
+            "cardImage": "reference within card",
+            "cardElixirCost": "reference within card"
+        },
+        {
+            "cardID": "reference object ID of card",
+            "cardImage": "reference within card",
+            "cardElixirCost": "reference within card"
+        },
+        {
+            "cardID": "reference object ID of card",
+            "cardImage": "reference within card",
+            "cardElixirCost": "reference within card"
+        }],
+        "averageCost": "calculate from cards' elixir costs",
+        "fourCardCycle": "calculate from cards' elixir costs"
+        },
+        "archetype": "Siege",
+        "postInfo": {
+            "overview": "3.0 Xbow Cycle is a fast-paced cycle deck that uses the Xbow as its main win condition. This deck has the clash royale xbow and tesla siege defense as well as the clash royale cycle cards for quick defense and mechanical gameplay. This is currently a very strong but high skill-cap clash royale deck. It is extremely similar to the clash royale 2.9 Xbow cycle deck, but has the clash royale Knight in place of the clash royale Ice Golem, which changes some interactions with different decks. The Knight's trait of being able to attack makes him favorable against several matchups, as compared to the passive Ice Golem.",
+            "strategy": "This deck is meant to be played passively. It is a punish deck, which means you need to be patient and strike at the perfect moment. Games will often go into overtime and even up to tiebreaker — Xbow is a tool to either bait out responses or punish mistakes, but one bad Xbow can lose you the game.",
+            "rating": 10,
+            "difficultyLevel": 5
+        },
+        "comments": [{
+            "userThatCommented": "refer",
+            "mainBody": "This deck is awesome! I love it, thank you for sharing! It is very hard to play, though...",
+            "userRating": 9,
+            "userDifficulty": 5,
+            "dateOfCreation": "2023-03-16",
+            "dateOfUpdation": null
+        },
+        {
+            "userThatCommented": "refer",
+            "mainBody": "This deck sucks. I much prefer a deck that actually has to cross the bridge to win a match.",
+            "userRating": 4,
+            "userDifficulty": 3,
+            "dateOfCreation": "2023-03-16",
+            "dateOfUpdation": null
+        },
+        {
+            "userThatCommented": "refer",
+            "mainBody": "It's an alright deck, though I would consider changing the Fireball to a Rocket. You can win easier with spell cycle!",
+            "userRating": 7,
+            "userDifficulty": 4,
+            "dateOfCreation": "2023-03-16",
+            "dateOfUpdation": "2023-03-16"
+        }]
+    }
