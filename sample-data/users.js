c9d26ff8275cf08233c099ba1f72468c2b873cd7
Shiv <shiviyer55x@gmail.com>
2023-03-17 14:44:51 +0800

    added new formatting

--- sample-data/users.js
+++ sample-data/users.js
@@ -1,6 +1,14 @@
+// old with particulars object
 users = {"_id": {"$oid":"6412b8f117e321eb2011910b"},
         "particulars":
             {"username":"mangostine",
             "email":"mangostine@gmail.com",
             "favoriteCard":"Musketeer"}
         }
+
+// new:
+user = {
+    "username": "chrihstine",
+    "email": "chrihstine@gmail.com",
+    "favoriteCard": "Three Musketeers"
+}
