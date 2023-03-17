a79c4e8ea7e1994b90795b73a405f2c84264d423
Shiv <shiviyer55x@gmail.com>
2023-03-17 14:23:29 +0800

    functional post request, THOUGH might need to refactor db format to remove particulars object

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 // set up express and app
+const { json } = require("express");
 const express = require("express");
 // require the env file, in order to safely retrieve the MongoDB URI. need to specify the path of the file, otherwise
 // will return undefined! require dotenv just finds a file called '.env' by default.
 require('dotenv').config();
 const app = express();
+const PORT = 3000;
+
+// app.use
+app.use(express.json());
 
 // retrieve URI (sensitive info) from .env file
 const mongoURI = process.env.MONGO_URI;
 // the database will be the main database to access
 const DATABASE = "RoyaleRavesDB";
-const COLLECTION = "posts";
+const POSTS_COLLECTION = "posts";
+const USERS_COLLECTION = "users";
 
 // for Express to talk to Mongo, we need a client
 // this client is for nodejs
@@ -17,51 +23,78 @@ const COLLECTION = "posts";
 // require("mongodb") returns an Object with many key/value pairs, we are only interested in MongoClient
  const MongoClient = require("mongodb").MongoClient;
 
-// main has to be an async function. we only want the routes to load once we are connected to the database
-async function main(){
+ // MongoClient.connect is async, so seeing as we await it, the connect function needs to be async
+async function connect(uri, dbName){
     // in order to connect to MongoDB, we need two pieces of information:
     // 1. a connection string
     // 2. a configuration object - in summary, this simplifies our access to MongoDB
-    // mongoclient.connect is async
-    const client = await MongoClient.connect(mongoURI,
-    {"useUnifiedTopology": true});
+    const client = await MongoClient.connect(uri, {"useUnifiedTopology": true});
+    const db = client.db(dbName);
+    return db;
+}
 
-    // mongo db to connect to
-    const db = client.db(DATABASE);
 
+// main has to be an async function. we only want the routes to load once we are connected to the database
+async function main(){
+    // mongo db to connect to, HAVE TO AWAIT
+    const db = await connect(mongoURI, DATABASE);
 
     // sanity test to see that our server works
     // first route, the home page
 
     // if we are doing await in app.get, the function needs to be an async function
     app.get("/", async function(req,res){
-        // log in the terminal
-        console.log("Hello world");
-        console.log("hi");
         // send any data to the server
-        res.send("YOOOOOooooo");
-        
+        //res.send("YOOOOOooooo");
+        console.log("req received");
         // select the first 3 documents
-        // equivalent: db.RoyaleRayesDB.find({}).limit(3)
+        // equivalent: db.posts.find({}).limit(3)
         // will take a while because it's an async function
-        const listings = await db.collection(COLLECTION)
+        const listings = await db.collection(USERS_COLLECTION)
         .find({})
         .limit(3)
         .toArray();
-        console.log(listings);
-    })
-}
 
-/*
-async function connect(uri, dbname){
-    let client = await MongoClient.connect(uri, {"useUnifiedTopology": true});
+        // console log the listings to ensure that it works
+        console.log(listings);
+        res.send(listings);
+    })
+
+    // async can be an ARROW FUNCTION
+    app.post("/", async (req, res) => {
+        console.log("req received")
+        // what must the document have? adding a new post test...
+        try{
+            const result = await db.collection(USERS_COLLECTION).insertOne({
+                // when posting the request, you have to get the values from the post request's body itself, not from the app
+                particulars:
+                {
+                    username: req.body.username,
+                    email: req.body.email,
+                    favoriteCard: req.body.favoriteCard
+                }
+            });
+            res.status(200);
+            // send to the response body
+            res.send(result);
+            //res.json({result:result})
+            console.log("Request sent!");
+            
+        } catch (e){
+            res.status(500);
+            res.send({
+                error: "Internal server error. Please contact Haikal."
+            });
+            console.log(e);
+        }
+    });
 }
-*/
 
 // call main before listening
 main();
 
+
 //  we can write listen first, so that we can ensure the route goes before
-app.listen(3000, function(){
-    console.log("Server has started");
+app.listen(PORT, function(){
+    console.log(`Server has started at http://localhost:${PORT}`);
 })
